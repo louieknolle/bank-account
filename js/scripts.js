748fc4b2259b26e2d6d2466a1e2b09c39037b8a1
Louis Knolle <knollelw@gmail.com>
2022-04-25 23:34:28 -0400

    add UI and two constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,47 @@
+// Business Logic for Bank Account
+function BankAccount() {
+  this.name = {};
+  this.balanceId = 0;
+}
+
+BankAccount.prototype.depositFunds = function(deposit) {
+  deposit.id = this.assignId();
+  this.name[NewAccount.id] = name; 
+};
+
+BankAccount.prototype.withdrawFunds = function(withdrawal) {
+
+};
+
+
+
+
+
+
+
+// Business Logic Funds
+function DepositWithdraw () {
+  this.deposit = {};
+  this.withdraw = 0;
+}
+
+// Business Logic for Name
+function NewAccount(name, initialDeposit, depositAmount, withdrawalAmount) {
+  this.name = name;
+  this.initialDeposit = initialDeposit; 
+}
+
+BankAccount.prototype.registration = function(newAccount) {
+  return this.name + this.initialDeposit; 
+}; 
+
+
+
+
+
+// UI Logic
+
 $(document).ready(function() {
   
 });
+
